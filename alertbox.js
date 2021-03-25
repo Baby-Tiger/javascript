9acc6ffe4f1d015efeb0a0da422d1d62883800d5
Baby-Tiger <17.alitariq@gmail.com>
2021-03-26 03:54:56 +0500

    Alert box Program added

---
+++ alertbox.js
@@ -0,0 +1,25 @@
+//alert box in javascript
+
+let firstVariable = 11 , secoundVariable = 22;
+
+if (firstVariable > secoundVariable) {
+
+    alert("First variable is greater then secound");
+
+} else {
+    
+    alert("Secound variable is greater then firts");
+
+}
+/*alertbox.js:5 Uncaught ReferenceError: secoundVariable is not defined
+    at alertbox.js:5
+(anonymous) @ alertbox.js:5
+Error Seen Not loading script.
+Its because its case sensitive.
+speeling mistake caught >>>>>  in decleration it was decleared as 
+secoundVAriable
+and in the condition i used secoundVariable 
+Small a after v 
+error removed
+run sucessfully
+*/
